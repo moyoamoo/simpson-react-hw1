ddb2e1f9db49428ad2409d15cdeb4ff3728009d3
Moyo A-P <moyo.amoo@hotmail.com>
2024-02-14 15:24:44 +0000

    spinner to functional

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -1,16 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import "./CSS/Spinner.css";
 
-class Spinner extends Component {
-  state = {};
-  render() {
-    return (
-      <div className="spinnerContainer">
-        <p className="spinnerText">Generating Characters</p>
-        <span className="loader"></span>
-      </div>
-    );
-  }
-}
+const Spinner = () => {
+  return (
+    <div className="spinnerContainer">
+      <p className="spinnerText">Generating Characters</p>
+      <span className="loader"></span>
+    </div>
+  );
+};
 
 export default Spinner;
